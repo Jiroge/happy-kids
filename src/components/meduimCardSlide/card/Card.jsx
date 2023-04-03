2247a82fa04785eb7e547562cb8dcc79806f5705
Jiroge <s.jiroge@gmail.com>
2023-04-03 21:23:46 +0700

    edit hover to make it correct

---
+++ src/components/meduimCardSlide/card/Card.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import "./Card.scss";
+function Card(props) {
+  const [hover, setHover] = useState(false);
+  const amountOfProduct = props.product.media.length;
+
+  return (
+    <div className="product-card">
+      <img
+        src={
+          props.product.media[amountOfProduct === 2 ? (hover ? 1 : 0) : 0].url
+        }
+        alt="index"
+        onMouseEnter={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+      />
+      {/* <p style={{display: hover ? null : "none", transition: "0.5s"}}>Quick View</p> */}
+      <div
+        className="quick-view"
+        onMouseEnter={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+      >
+        {hover && <p>Quick View</p>}
+      </div>
+      <br />
+      <p className="product-name">{props.product.name}</p>
+      <p className="product-price">{props.product.price}₺</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+export default Card;
